336c79b741de079af04f1b4117f937c722a8d6dd
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-09-05 20:52:37 -0300

    Criação de Menu para Inserção de funcionário, listagem dos cadastrados com seus dados e saída do sistema. Entrega Média.

--- ListaFuncionarios.java
+++ ListaFuncionarios.java
@@ -6,28 +6,58 @@ public class ListaFuncionarios {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         System.out.println("Bem-vindo! Sistema Cadastral de Funcionários\n");
-
-        //Criação de HashMap para receber lista de funcionários
+        System.out.println("Escolha uma das opções abaixo e digite o número correspondente: \n");
         Map<String, String> funcionarios = new HashMap<String, String>();
+        int opcaoDigitada = 0;
+        boolean looping = true;
 
-        //Entrada dos dados para um funcionário
-        System.out.println("Se deseja CADASTRAR um funcionário, entre com os dados abaixo. ");
-        System.out.println("Entre com o nome do Funcionário: ");
-        String nomeDoFuncionario = teclado.nextLine();
-        System.out.println("Entre com o CPF do Funcionário: ");
-        String cpf = teclado.nextLine();
-        System.out.println("Entre com o telefone do Funcionário: ");
-        String telefone = teclado.nextLine();
-        System.out.println("Entre com o email do Funcionário: ");
-        String email = teclado.nextLine();
-        funcionarios.put(cpf, " Nome: " + nomeDoFuncionario + ". Telefone: " + telefone + ". E-mail: " + email);
-        System.out.println(" ");
-        System.out.println("Usuário Cadastrado. Operação realizada com sucesso.");
-        //Estrutura para saída melhor formatada
-        for (String referencia : funcionarios.values()) {
-            System.out.println(referencia);
+        //Menu para escolha de entrada, listagem ou saída do sistema
+        while (looping == true) {
+            System.out.println("Se deseja CADASTRAR um funcionário, digite 1. ");
+            System.out.println("Se deseja MOSTRAR o(s) funcionário(s) cadastrados, digite 2. ");
+            System.out.println("Se deseja SAIR, digite 3. ");
+            System.out.println(" ");
+            System.out.println("Digite a opção desejada: ");
+
+            opcaoDigitada = teclado.nextInt();
+
+            //Menu de escolha com validação
+            switch (opcaoDigitada) {
+                case 1: {
+                    teclado.nextLine();
+                    System.out.println("Entre com o nome do Funcionário: ");
+                    String nomeDoFuncionario = teclado.nextLine();
+                    System.out.println("Entre com o CPF do Funcionário: ");
+                    String cpf = teclado.nextLine();
+                    System.out.println("Entre com o telefone do Funcionário: ");
+                    String telefone = teclado.nextLine();
+                    System.out.println("Entre com o email do Funcionário: ");
+                    String email = teclado.nextLine();
+                    funcionarios.put(cpf, "Nome: " + nomeDoFuncionario + ". Telefone: " + telefone + ". E-mail: " + email);
+                    System.out.println(" ");
+                    System.out.println("Usuário Cadastrado. Operação realizada com sucesso.");
+                    break;
+                }
+                case 2: {
+                    for (String referencia : funcionarios.keySet()) {
+                        System.out.println(funcionarios.get(referencia) + ". CPF: " + referencia);
+                    }
+                    break;
+                }
+                case 3: {
+                    looping = false;
+                    System.out.println("Você saiu do sistema");
+                    break;
+                }
+                default: {
+                    System.out.println("Digite uma opção válida.\n");
+                    break;
+                }
+            }
         }
-        //Entrega mínima
 
+        teclado.close();
     }
 }
+//Criação de Menu para Inserção de funcionário, listagem dos cadastrados com seus dados e saída do sistema
+//Entrega Média
